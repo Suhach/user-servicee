c21acf451700dc8e1eec88aaaa8358dd8ba27c56
Examples <61053710+Suhach@users.noreply.github.com>
2025-05-10 17:50:29 +0300

    Update model.go

--- internal/user/model.go
+++ internal/user/model.go
@@ -6,7 +6,6 @@ type User struct {
 	ID    int    `gorm:"primaryKey"`
 	Email string `gorm:"type:varchar(255);"`
 	Pass  string `gorm:"type:varchar(255);"`
-	//Tasks []Task `gorm:"foreignKey:user_id"`
 	CreatedAt time.Time  `gorm:"type:timestamptz;"`
 	UpdatedAt time.Time  `gorm:"type:timestamptz;"`
 	DeletedAt *time.Time `gorm:"type:timestamptz;"`
